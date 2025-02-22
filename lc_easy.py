fdec1fa4c792d21d2168c6052adc0427e58f781d
Karthik Babu H <hkarthikbabu@yahoo.com>
2025-02-22 12:02:34 -0800

    latest solved

--- lc_easy.py
+++ lc_easy.py
@@ -244,15 +244,260 @@
 # prices = [7, 1, 5, 3, 6, 4]
 # print(maxProfit(prices))
 
-def getIntersectionNode(heada, headb):
-    if not heada or not headb:
-        return Null
+# def getIntersectionNode(heada, headb):
+#     if not heada or not headb:
+#         return Null
+#
+#     one = heada
+#     two = headb
+#
+#     while one != two:
+#         one = one.next if one else headb
+#         two = two.next if two else heada
+#
+#     return one
 
-    one = heada
-    two = headb
+# Majority Element
+# def maj_ele(nums):
+#     d = {}
+#     n = len(nums)
+#
+#     for num in nums:
+#         d[num] = d.get(num, 0) + 1
+#         if d[num] > n//2:
+#             return num
+#
+# nums = [2,2,1,1,1,2,2]
+# print(maj_ele(nums))
 
-    while one != two:
-        one = one.next if one else headb
-        two = two.next if two else heada
+# Rotate Array
+# def rot_arr(nums, k):
+#     n = len(nums)
+#     k = k % n
+#     nums.reverse()
+#     nums[:k] = reversed(nums[:k])
+#     nums[k:] = reversed(nums[k:])
+#     return nums
+#
+# nums = [1,2,3,4,5,6,7]
+# k = 3
+# print(rot_arr(nums, k))
 
-    return one
+# def find_missing_ranges(nums, lower, upper):
+#     res = []
+#     print(nums)
+#     nums = [lower - 1] + nums + [upper + 1]
+#     print(nums)
+#
+#     for i in range(1, len(nums)):
+#         if nums[i] - nums[i - 1] > 1:
+#             # If the gap is more than 1, we have a missing range
+#             start = nums[i - 1] + 1
+#             end = nums[i] - 1
+#             if start == end:
+#                 res.append(str(start))  # Single number
+#             else:
+#                 res.append(f"{start}->{end}")  # Range
+#     return res
+#
+# nums = [0, 1, 3, 50, 75]
+# lower = 0
+# upper = 99
+# print(find_missing_ranges(nums, lower, upper))
+
+# def comb_sum(candidates, target):
+#     def backtrack(start, target, path):
+#         if target == 0:
+#             res.append(path[:])
+#             return
+#         if target < 0:
+#             return
+#
+#         for i in range(start, len(candidates)):
+#             if candidates[i] > target:
+#                 continue
+#             path.append(candidates[i])
+#             backtrack(i, target-candidates[i], path)
+#             path.pop()
+#
+#     res = []
+#     backtrack(0, target, [])
+#     return res
+#
+# candidates = [2, 3, 6, 7]
+# target = 7
+# print(comb_sum(candidates, target))
+
+# def prod_arr(nums):
+#     n = len(nums)
+#     res = [1] * (n)
+#
+#     left = 1
+#     for i in range(n):
+#         res[i] = left
+#         left *= nums[i]
+#
+#     right = 1
+#     for i in range(n-1, -1, -1):
+#         res[i] *= right
+#         right *= nums[i]
+#     return res
+#
+# nums = [1, 2, 3, 4]
+# print(prod_arr(nums))
+
+# Reverse Bits
+# def reverseBits(n):
+#     res = 0
+#     for i in range(32):
+#         bit = n & 1
+#         res = (res << 1) | bit
+#         n >>= 1
+#     return res
+# n = 43261596
+# print(reverseBits(n))
+
+# Number of 1 Bits
+# def hammingWeight(n):
+#     count = 0
+#     while n:
+#         count += n & 1
+#         n >>= 1
+#     return count
+#
+# n = 11
+# print(hammingWeight(n))
+
+# def isHappy(n):
+#     visited = set()
+#     while n not in visited:
+#         visited.add(n)
+#         n = sumofsq(n)
+#         if n == 1:
+#             return True
+#     return False
+#
+# def sumofsq(n):
+#     output = 0
+#     while n:
+#         digit = n % 10
+#         digit = digit**2
+#         output += digit
+#         n = n//10
+#     return output
+#
+# n = 19
+# print(isHappy(n))
+
+# Reverse Linked List
+# def reverseList(head):
+#     curr = head
+#     prev = None
+#     next = None
+#
+#     while curr:
+#         next = curr.next
+#         curr.next = prev
+#         prev = curr
+#         curr = next
+#     return prev
+
+# def strStr(haystack, needle):
+#     if not needle:
+#         return 0
+#
+#     for i in range(len(haystack) -len(needle)+1):
+#         if haystack[i:i+len(needle)] == needle:
+#             print(haystack[i:i+len(needle)])
+#             return i
+#
+# haystack = "hello"
+# needle = "ll"
+# print(strStr(haystack, needle))
+
+# Remove Element
+# def removeElement(nums,val):
+#     pos = 0
+#     for i in range(len(nums)-1):
+#         if nums[i] != val:
+#             nums[pos] = nums[i]
+#             pos += 1
+#     return nums[:pos]
+#
+# nums = [3, 2, 2, 3]
+# val = 3
+# print(removeElement(nums, val))
+
+from collections import Counter
+
+# def intersect(nums1, nums2):
+#     count1 = Counter(nums1)
+#     count2 = Counter(nums2)
+#     result = []
+#     print(count1, count2)
+#
+#     for num in count1:
+#         if num in count2:
+#             result.extend([num] * min(count1[num], count2[num]))
+#
+#     return result
+#
+# nums1 = [1, 2, 2, 1]
+# nums2 = [2, 2]
+# print(intersect(nums1, nums2))
+
+# from collections import Counter
+# def firstUniqChar(s):
+#     count = Counter(s)
+#     print(count)
+#     d = {}
+#     for c in s:
+#         d[c] = d.get(c,0) + 1
+#
+#     for i,v in enumerate(s):
+#         if d[v] == 1:
+#             return i
+#
+#
+# s = "leetcode"
+# print(firstUniqChar(s))
+
+# def fizzBuzz(n):
+#     res = []
+#     for i in range(1, n+1):
+#         if i % 3 == 0:
+#             res.append("Fizz")
+#         elif i % 5 == 0:
+#             res.append("Buzz")
+#         elif i % 3 == 0 and i % 5 == 0:
+#             res.append("FizzBuzz")
+#         else:
+#             res.append(str(i))
+#     return res
+#
+# n = 5
+# print(fizzBuzz(n))
+
+# def pow(x,n):
+#     return (x**n)
+#
+# x = 2.0
+# n = 10
+# print(pow(x,n))
+
+# Move Zeroes
+
+from collections import Counter
+# def moveZeroes(nums):
+#     index = 0
+#     for i in range(len(nums)):
+#         if nums[i] != 0:
+#             nums[index] = nums[i]
+#             index += 1
+#
+#     for i in range(index, len(nums)):
+#         nums[i] = 0
+#     return nums
+#
+# nums = [0,1,0,3,12]
+# print(moveZeroes(nums))
